30381eba8b07708afc7fbde013b981ee152bba77
JulianCRA <jj.juliancr@gmail.com>
2018-04-11 15:35:07 +0700

    Rudimentary flood fill algorithm functioning.

--- script/flood-fill.js
+++ script/flood-fill.js
@@ -13,16 +13,16 @@ var floodfill = function (p){
     p.grid;
     let colors;
     let seed
-    let pixStack;
+    p.pixStack;
     
     p.setup = function(){
-        p.initSketch(300, 300, 600, 2);
+        p.initSketch(200, 200, 600, 2);
     }
 
     p.initSketch = function(w, h, r, c){
         canvas = p.createCanvas(600,600);
         canvas.doubleClicked = function(){p.doubleClicked();}
-        canvas.mouseClicked(fFill);
+        canvas.mouseClicked(startFill);
         p.noLoop();
         p.noStroke();
         
@@ -36,7 +36,7 @@ var floodfill = function (p){
         colors = new Array(gridWidth);
         p.grid = new Grid(gridWidth, gridHeight);
         seed = new Array();
-        pixStack = new Array();
+        p.pixStack = new Array();
 
         for(let i = 0; i < colorList.length; i++){
             colorList[i] = p.color(p.random(255),p.random(255),p.random(255));
@@ -46,18 +46,73 @@ var floodfill = function (p){
             let rColor = Math.floor(Math.random()*colorList.length);
             let xpos = Math.floor(Math.random()*gridWidth);
             let ypos = Math.floor(Math.random()*gridHeight);
-            seed.push({x:xpos, y:ypos, c:colorList[rColor]});
-            p.grid.next[xpos][ypos] = rColor + 1;
+            seed.push({x:xpos, y:ypos});
+            p.grid.current[xpos][ypos] = rColor + 1;
         }
-        p.grid.iterateAll();
         p.voronoi(seed, false);
     }
 
-    fFill = function(){
+    startFill = function(){
         let startX = Math.floor(p.mouseX/cellWidth);
         let startY = Math.floor(p.mouseY/cellHeight);
-        pixStack.push({x:startX, y:startY});
-        console.log(startX, startY);
+        floodFill(startX, startY, p.grid.current[startX][startY], 0);
+    }
+
+    floodFill = function(xpos, ypos, startState, newState){
+        if(startState!=newState)
+            p.pixStack.push({x:xpos, y:ypos});
+        
+        while(p.pixStack.length > 0){
+            //console.log("tiki");
+            let searchLeft = false;
+            let searchRight = false;
+            let newPos = p.pixStack.pop();
+
+            while(newPos.y > 0){
+                newPos.y--;
+                if(p.grid.current[newPos.x][newPos.y] != startState){
+                    newPos.y++;
+                    break;
+                }
+            }
+            
+            while(newPos.y < gridHeight){
+                if(p.grid.current[newPos.x][newPos.y] == startState){
+                    p.grid.current[newPos.x][newPos.y] = newState;
+                    
+                    if(newPos.x > 0){
+                        if(p.grid.current[newPos.x-1][newPos.y] == startState){
+                            if(!searchLeft){
+                                p.pixStack.push({x:newPos.x-1, y:newPos.y});
+                                searchLeft = true;
+                            }
+                        }
+                        else if(searchLeft){
+                            searchLeft = false;
+                        }
+                    }
+
+                    if(newPos.x < gridWidth -1){
+                        if(p.grid.current[newPos.x+1][newPos.y] == startState){
+                            if(!searchRight){
+                                p.pixStack.push({x:newPos.x+1, y:newPos.y});
+                                searchRight = true;
+                            }
+                        }
+                        else if(searchRight){
+                            searchRight = false;
+                        }
+                    }
+
+                }
+                else{
+                    newPos.y--;
+                    break;
+                }
+                newPos.y++;
+            }
+        }
+        p.redraw();
     }
 
     p.draw = function(){
@@ -65,14 +120,18 @@ var floodfill = function (p){
         
         for(let i = 0; i < gridWidth; i++){
             for(let j = 0; j < gridHeight; j++){
-                p.fill(colors[i][j]);
+                if(p.grid.current[i][j] == 0)
+                    p.fill('black');
+                else
+                    p.fill(colorList[p.grid.current[i][j]-1]);
+
                 p.rect(i*cellWidth, j*cellHeight, cellWidth, cellHeight);
             }
         }
-        /*p.fill(255);
+        p.fill(255);
         for(let i = 0; i < seed.length; i++){
-            p.ellipse(seed[i].x*cellWidth, seed[i].y*cellHeight, 10, 10);
-        }*/
+            p.ellipse((seed[i].x+0.5)*cellWidth, (seed[i].y+0.5)*cellHeight, 10, 10);
+        }
     }
 
     p.voronoi = function(origin, euclidean = true){
@@ -95,7 +154,7 @@ var floodfill = function (p){
                         distance = dd;
                     }
                 }
-                colors[i][j] = closest.c;
+                p.grid.current[i][j] = p.grid.current[closest.x][closest.y];
             }
         }
     }
